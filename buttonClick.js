622a6cd7e04090f1c6fbcfc937e9d7ecb853e587
Treefa88 <treefa88@gmail.com>
2019-07-25 15:47:28 +0000

    Alert modified

--- buttonClick.js
+++ buttonClick.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 				employeeRole: roleText
 			},
 			success: function(data){
-				alert("Data sent!");
+				alert("Data sent successfully!");
 			},
 			error: function(xhr, ajaxOptions, thrownError){
 				alert(xhr.responseText + "\n" + xhr.status + "\n" + thrownError);
