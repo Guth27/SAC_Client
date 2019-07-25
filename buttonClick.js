33153731024873ab0bb4ebc291481f77bb98d220
Guth27 <guth27@gmail.com>
2019-07-25 12:27:58 +0200

    Client base

---
+++ buttonClick.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	//event.preventDefault();
+	$("#get").click(function(){
+		var idText = document.getElementById("outputTextId").value;
+		document.getElementById("outputTextId").value = "";
+		
+		//alert(idText);
+		$.ajax({
+			url: "http://localhost:8080/???/rest/employees/find",
+			type: "get",
+			data:{
+				id: idText 
+			},
+			success: function(data){
+				$(".outputDataText").append("Name:" + data.name + ", role:" + data.role);
+			},
+			error: function(xhr, ajaxOptions, thrownError){
+				alert(xhr.responseText + "\n" + xhr.status + "\n" + thrownError);
+			}
+		});
+		
+	});
+	$("#post").click(function(){
+		var nameText = document.getElementById("inputTextName").value;
+		var roleText = document.getElementById("inputTextRole").value;
+		document.getElementById("inputTextName").value = "";
+		document.getElementById("inputTextRole").value = "";
+		document.getElementById("outputP").innerHTML = "Output:";
+		//alert(nameText + roleText);
+		$.ajax({
+			url: "http://localhost:8080/???/rest/employees/insert",
+			type: "post",
+			data:{
+				employeeName: nameText,
+				employeeRole: roleText
+			},
+			success: function(data){
+				alert("Data sent!");
+			},
+			error: function(xhr, ajaxOptions, thrownError){
+				alert(xhr.responseText + "\n" + xhr.status + "\n" + thrownError);
+			}
+		});
+	});
+});
