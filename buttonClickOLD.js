82f2ecb7e6cf5ae3dd0852d01eb2152f800f5e27
Treefa88 <treefa88@gmail.com>
2019-07-29 12:00:33 +0000

    Old

--- buttonClickOLD.js
+++ buttonClickOLD.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 				employeeRole: roleText
 			},
 			success: function(data){
-				alert("Data sent successfully!");
+				alert("Data sent successfully (Old)!");
 			},
 			error: function(xhr, ajaxOptions, thrownError){
 				alert(xhr.responseText + "\n" + xhr.status + "\n" + thrownError);
