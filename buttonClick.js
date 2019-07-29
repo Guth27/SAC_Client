b2eeed44e2b08b0f247f6d94cde718187e974f0d
Treefa88 <treefa88@gmail.com>
2019-07-29 12:04:01 +0000

    Actual 2.0

--- buttonClick.js
+++ buttonClick.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 				employeeRole: roleText
 			},
 			success: function(data){
-				alert("Data sent successfully (Actual)!");
+				alert("Data sent successfully (Actual 2.0)!");
 			},
 			error: function(xhr, ajaxOptions, thrownError){
 				alert(xhr.responseText + "\n" + xhr.status + "\n" + thrownError);
